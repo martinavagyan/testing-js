ccf5db3cec473261b8103592e416b4f64a71e7c8
martinavagyan <martinavagyan@gmail.com>
2017-05-21 12:39:35 +0200

    files copied

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports = Bear;
+module.exports = Bear
 
 function Bear (type) {
-  this.type = type || 'any';
+  this.type = type || 'any'
 }
 Bear.prototype.growl = function (says) {
-  return 'The ' + this.type + ' bear says ' + (says || 'grrr');
+  return 'The ' + this.type + ' bear says ' + (says || 'grrr')
 }
